e240b7b641fa4727766973261257faff1aeacfaf
unknown <michaelaguilar89>
2025-02-19 13:45:03 -0600

    fixing bug cloud connection for railway

--- obj/Debug/net8.0/BlazorAppCarsCrud.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorAppCarsCrud.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppCarsCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5da7c61810315881de469e32ce7936679c0f6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07a325e3e79fce8e9d143c507b3f4e18dc976c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppCarsCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppCarsCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
